bfd7e9d52eb66b33a8003071f9e752424cc42609
Akil Harris <akil.harris@gmail.com>
2016-03-02 22:48:32 -0500

    responds to a query on /search.json

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
-                 [clojurewerkz/elastisch "2.2.1"]]
+                 [clojurewerkz/elastisch "2.2.1"]
+                 [ring/ring-json "0.4.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler vice-search.handler/app}
   :profiles
--- src/vice_search/handler.clj
+++ src/vice_search/handler.clj
@@ -1,11 +1,19 @@
 (ns vice-search.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.json :as ring-json]
+            [ring.util.response :as rr]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] ":8(")
+  (GET "/search.json" [q]
+    (rr/response {:results [{:id "test" :val q}]}))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (wrap-defaults site-defaults)
+      (ring-json/wrap-json-response)))
+; (def app
+;   (wrap-defaults app-routes site-defaults))
